c280c294d2737213f088278cf66267de813a9929
miguelsilva1234 <miguel.silva233@etec.sp.gov.br>
2025-08-01 18:13:39 -0300

    Envio inicial

---
+++ primeiro-teste/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MEU APP REACT</h1>
+      <p>nfenfhufuhfw</p>
+    </div>
+  );
+}
+
+export default App;
